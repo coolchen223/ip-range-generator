514dc8b8d7afce73c4b641c32a23a19cc86a9f6f
Nicholas Wold <nickw254@gmail.com>
2014-06-27 12:34:28 -0700

    Added ability to go through the actual hostnames

--- generate_ranges.py
+++ generate_ranges.py
@@ -2,31 +2,48 @@
 # Script to take in a subnet and prune out the unnecessary IPs for Nessus purposes
 # Written by Samuel Zhu
 
-import socket
 import sys
+import socket
+from netaddr import *
 
-def gen_range(subnet):
-    ip_addr = subnet.split("/")[0] #split input on subnet slash
-    ip_addr = ".".join(ip_addr.split(".")[:-1]) + ".2"
-    #ip_addr = ip_addr[:-1] + "2"   #grab everything except the last digit (works on /24 subnets)
+def real_range(subnet, debug):
+    ip_range = IPNetwork(subnet)
+    ip_list = list(ip_range) # Creates a list of all IPs in the range given.
+    ret = []
+    if debug:
+        print "Looking at %s (%s addresses)..." % (subnet, len(ip_list))
+    for i in ip_list:
+        try:
+            if debug:
+                print "---"
+                print "%s" % i
+            name = socket.gethostbyaddr(str(i))[0]
+            if debug:
+                print name
+            if "inr" in name:
+                raise
+            elif "ishmael" in name:
+                raise
+            else:
+                print "hostname valid!"
+                ret.append(i)
+        except:
+            if debug:
+                print "hostname not valid!"
+            # If it's an end of the list, assume that it's not active and cut it out.
+            # If it's in the middle of the range, then it's more likely a connection error and it won't matter
+            # if we cut it out.
 
+    return str(ret[0]) + "-" + str(ret[-1])
 
-    try:
-        hostname = socket.gethostbyaddr(ip_addr)[0]
-        ret = ip_addr[:-1] + "4" if "inr" in hostname else ip_addr #fancy ternary statement to put correct start of range
-    except:
-        ret = ip_addr
-
-    ip_addr = ip_addr[:-1] + "254"
-    try:
-        hostname = socket.gethostbyaddr(ip_addr)[0]
-        ret += "-" + ip_addr[:-3] + "253" if "ishmael" in hostname else "-" + ip_addr #other fancy ternary statement
-    except:
-        ret += "-" + ip_addr
-    return ret
-
-f = open(sys.argv[1], 'r')
+debug = False
+for a in sys.argv:
+    if 'debug' in a:
+        print "DEBUG ON"
+        debug = True
+        
+f = open(sys.argv[-1], 'r')
 
 for line in f:
-    print gen_range(line)
+    print real_range(line, debug)
 
